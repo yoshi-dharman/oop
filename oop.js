4ee0687382f2b0eed6cea439a6e6dcefac3a5a3b
yoshi-dharman <yoshi.dharman@ti.ukdw.ac.id>
2021-02-15 16:47:20 +0700

    add more

--- oop.js
+++ oop.js
@@ -15,37 +15,62 @@ class Enemy{
         console.log("Confused");
     }
     
+    attacked(damage){
+        this.healthPoint -= damage;
+        console.log(`${this.name} attacked, hp - ${damage}`);
+        console.log(`${this.name} hp remain = ${this.healthPoint}`);
+        if (this.healthPoint <=0){
+            this.isDead = true;
+            console.log(`${this.name} is Dead`);
+        }
+        
+    }
 }
 
 class Pocong extends Enemy{
+    constructor(name, speed, healthPoint, attackPoint, isDead){
+        super(name, speed, healthPoint, attackPoint, isDead);
+    }
+
     jump(){
-        console.log("Jump!");
+        console.log(`${this.name} Jump!`);
     }
 
     supperJump(){
-        console.log("Supper Jump!");
+        console.log(`${this.name} Supper Jump!`);
     }
 
     attack(){
-        console.log("Pocong Attack!");
+        console.log(`${this.name} Attack!`);
     }
+
 }
 
 class Zombie extends Enemy{
     walk(){
-        console.log("Walk!");
+        if(this.speed == 20){
+            this.speed -= 10;
+            console.log(`${this.name} turn to walk from running`);
+        }
+        console.log(`${this.name} Walk!`);
     }
 
     running(){
-        console.log("Running!");
+        if(this.speed == 20){
+            console.log(`${this.name} already at top speed`);
+        }
+        else{
+            this.speed += 10;
+            console.log(`${this.name} Running!`);
+        }
     }
 
     attack(){
-        console.log("Zombie Attack!");
+        console.log(`${this.name} Zombie Horde with ${this.attackPoint} attack point!`);
     }
 
     infected(){
-        console.log("Palague!!");
+        console.log(`${this.name} Palague!!`);
     }
 }
 
@@ -54,3 +79,7 @@ let popocong = new Pocong("Popo", 10, 150, 10, false);
 
 zombie_man.attack();
 popocong.supperJump();
+
+zombie_man.attacked(30);
+zombie_man.attacked(15);
+zombie_man.attacked(100);
